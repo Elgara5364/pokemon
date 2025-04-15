757beed4b176aff7c84a542907691b5bb34140bf
Elgara5364 <steveloardi5364@gmail.com>
2025-04-15 11:46:33 +0900

    add path to index.ejs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
@@ -16,7 +19,7 @@ const API = {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route definitions
-app.get("/", (req, res) => res.render("index.ejs"));
+app.get("/", (req, res) => res.render(__dirname + "./views/index.ejs"));
 app.post("/get-pokemon", handlePokemonRequest);
 
 // Start server
