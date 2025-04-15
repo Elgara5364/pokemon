fdfbfc7db495ec6f32759fc31868ec4999cd675a
Elgara5364 <steveloardi5364@gmail.com>
2025-04-15 12:00:56 +0900

    add url domain

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import * as path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const DOMAIN = "https://pokemon-snowy-ten.vercel.app/";
 
 // API endpoints
 const API = {
@@ -19,11 +17,9 @@ const API = {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route definitions
-app.set("view engine", "ejs");
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.set("views", path.join(__dirname, "views"));
-app.get("/", (req, res) => res.render("index.ejs"));
-app.post("/get-pokemon", handlePokemonRequest);
+
+app.get(DOMAIN, (req, res) => res.render("index.ejs"));
+app.post(DOMAIN + "get-pokemon", handlePokemonRequest);
 
 // Start server
 app.listen(port, () => console.log(`Server running on port: ${port}`));
